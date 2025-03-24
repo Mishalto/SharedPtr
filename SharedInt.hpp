b7dcae70959e917af6705a3454ca3754b11346f9
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-24 15:31:51 +0300

    templates added

--- SharedInt.hpp
+++ SharedInt.hpp
@@ -1,59 +1,50 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <string_view>
-
-class SharedInt {
+template <typename T>
+class SharedPtr {
    private:
-    int* counter_;
-    int* obj_;
+
+    size_t counter_;
+    T* obj_;
 
     std::string name_;
     void print_counter() const {
         if (counter_) {
-            std::cout << name_ << ": &" << *counter_ << '\n';
+            std::cout << name_ << ": &" << counter_ << '\n';
         } else {
             std::cout << name_ << ": &0" << '\n';
         }
     }
 
    public:
-    SharedInt(std::string_view name) : name_(name) {
+    SharedPtr(std::string_view name) : name_(name) {
         obj_ = nullptr;
-        counter_ = nullptr;
+        counter_ = 0;
         std::cout << name_ << " -> Default constructor.\n";
         print_counter();
     };
 
-    ~SharedInt() {
-        if (counter_) {
-            if (--(*counter_) == 0) {
-                delete obj_;
-                delete counter_;
-                std::cout << name_ << " -> destructed.\n";
-            }
-        } else {
-            std::cout << "Fake destructor, no data to clean.\n";
-        }
-    }
-
-    SharedInt(std::string_view name, int obj) : name_(name) {
-        obj_ = new int(obj);
-        counter_ = new int(1);
+    SharedPtr(std::string_view name, T obj) : name_(name) {
+        obj_ = new T(obj);
+        counter_ = 1;
         std::cout << name_ << "-> constructor with parameters.\n";
         print_counter();
     }
+    ~SharedPtr() {
+        if (--counter_ == 0) {
+            delete obj_;
+            std::cout << name_ << " -> destructed.\n";
+        }
+    }
 
-    SharedInt(const SharedInt& other) {
+    SharedPtr(const SharedPtr& other) {
         counter_ = other.counter_;
         obj_ = other.obj_;
         name_ = other.name_;
-        if (counter_) {
-            ++(*counter_);
-        }
+
+        ++counter_;
+
         print_counter();
         std::cout << name_ << "-> copy constructor.\n";
     }
 
-    int& operator*() { return *obj_; }
+    T& operator*() { return *obj_; }
 };
